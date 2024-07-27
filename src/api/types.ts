373a96d7a1ae87d6334afe027981794a169dddea
Madison Saylor <maddie0264@gmail.com>
2024-07-27 18:37:07 -0400

    fix twitch.ts error

--- src/api/types.ts
+++ src/api/types.ts
@@ -1,4 +1,4 @@
-import type { Game as IgdbGame } from "igdb-api-types"
+import type { Game as IgdbGame } from 'igdb-api-types';
 
 export type Game = IgdbGame & {
   coverUrl?: string;
@@ -7,16 +7,16 @@ export type Game = IgdbGame & {
   userRating?: number;
 }
 
-export type TwitchGamesResponse = {
-  data: TwitchGame[];
-  pagination: {
-    cursor: string;
-  };
-}
-
 export type TwitchGame = {
   id: string;
   name: string;
   box_art_url: string;
   igdb_id: string;
+}
+
+export type TwitchGamesResponse = {
+  data: TwitchGame[];
+  pagination: {
+    cursor: string;
+  };
 }
