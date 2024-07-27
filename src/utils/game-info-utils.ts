4a7b88164a81619b4ff9c9273ff8425a5f511b92
m-saylor <maddie0264@gmail.com>
2024-07-27 12:59:46 -0700

    game-info-utils

--- src/utils/game-info-utils.ts
+++ src/utils/game-info-utils.ts
@@ -1,10 +1,17 @@
+import type { Game as IgdbGame } from "igdb-api-types"
+
 export function getCoverUrl(url: string | undefined, size: string) {
   if (!url) return "";
   const coverUrl = `https:${url}`.replace('thumb', size);
   return coverUrl;
 }
 
-export function getFirstReleaseYear(game, years) {
-  const first = years[game?.release_dates?.[0]];
-  return first;
+export function getFirstReleaseYear(game: IgdbGame, years: { [key: number]: number | undefined }) {
+  const firstReleaseDate = game?.release_dates?.[0]
+
+  if (typeof firstReleaseDate === "number") {
+    return years[firstReleaseDate];
+  }
+  
+  return undefined;
 }
